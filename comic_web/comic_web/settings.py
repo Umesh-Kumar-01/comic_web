5740f19e227cbcd101dd599834b622421080bfb2
Umesh Kumar <umeshkumar26122002@gmail.com>
2023-11-26 06:16:53 +0530

    more secure

--- comic_web/comic_web/settings.py
+++ comic_web/comic_web/settings.py
@@ -28,6 +28,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 ALLOWED_HOSTS = []
 
 
